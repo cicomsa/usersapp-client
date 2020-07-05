930e78aad5e35f2c3d6869601f91e52a5d5626af
cicomsa <ioanacomsa85@gmail.com>
2020-07-05 13:15:22 +0100

    Updates

---
+++ precache-manifest.4375ff895963e2eab79b405e49e54ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e2e4f31b28c532544493a0de604d5c",
+    "url": "/usersapp-client/index.html"
+  },
+  {
+    "revision": "5dbd3fa4c80410facfb2",
+    "url": "/usersapp-client/static/css/main.410257a3.chunk.css"
+  },
+  {
+    "revision": "4a05d9eeaf44f0611cd5",
+    "url": "/usersapp-client/static/js/2.66caaf05.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/usersapp-client/static/js/2.66caaf05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbd3fa4c80410facfb2",
+    "url": "/usersapp-client/static/js/main.53b77d2b.chunk.js"
+  },
+  {
+    "revision": "1fe91309d91e86e2c024",
+    "url": "/usersapp-client/static/js/runtime-main.5eb2e195.js"
+  }
+]);
